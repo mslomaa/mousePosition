20b55f6b5bbb834eefd25c02e6b2de197360cb66
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-17 13:58:39 +0200

    bcg change color

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ const h1 = document.querySelector('h1');
 document.body.addEventListener('mousemove', () => {
     // console.log(event.clientX, event.clientY);
     h1.textContent = `${event.clientX}, ${event.clientY}`;
+
+    document.body.style.backgroundColor = ``
 })
