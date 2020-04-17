9b8f725ffdd2c2c86e4ee897a1d2c77db2796e9d
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-17 11:58:06 +0200

    add clientX and clientY

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const h1 = document.querySelector('h1');
 document.body.addEventListener('mousemove', () => {
-    
+    // console.log(event.clientX, event.clientY);
+    h1.textContent = `${event.clientX}, ${event.clientY}`;
 })
