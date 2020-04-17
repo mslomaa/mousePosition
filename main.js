3cda69e3c6fd3ebd6720e4769765501bb0258b5c
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-17 14:45:27 +0200

    fixed color

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const h1 = document.querySelector('h1');
 document.body.addEventListener('mousemove', () => {
-    // console.log(event.clientX, event.clientY);
-    h1.textContent = `${event.clientX}, ${event.clientY}`;
+    const x = event.clientX;
+    const y = event.clientY;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    h1.textContent = x + ', ' + y;
 
-    document.body.style.backgroundColor = `rgb(${event.clientX*2}, ${event.clientY}, ${event.clientX/event.clientY*30})`
+    const red = x / width * 100;
+    const green = y / height * 100;
+    const blue = ((x / width * 100) + (y / height * 100)) / 2;
+
+    document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`
 })
