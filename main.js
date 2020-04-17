4141b0c7982c08fae5d094ee04803fad6b443efe
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-17 18:31:57 +0200

    oder change color

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
+// const h1 = document.querySelector('h1');
+// document.body.addEventListener('mousemove', () => {
+//     const x = event.clientX;
+//     const y = event.clientY;
+//     const width = window.innerWidth;
+//     const height = window.innerHeight;
+//     h1.textContent = x + ', ' + y;
+
+//     const red = x / width * 100;
+//     const green = y / height * 100;
+//     const blue = ((x / width * 100) + (y / height * 100)) / 2;
+
+//     document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`
+// })
+
 const h1 = document.querySelector('h1');
-document.body.addEventListener('mousemove', () => {
+
+document.body.addEventListener('click', () => {
     const x = event.clientX;
     const y = event.clientY;
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    h1.textContent = x + ', ' + y;
 
-    const red = x / width * 100;
-    const green = y / height * 100;
-    const blue = ((x / width * 100) + (y / height * 100)) / 2;
-
-    document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`
+    if (x % 2 === 0 && y % 2 === 0){
+        document.body.style.backgroundColor = 'red';
+        console.log(x, y);
+    } else if (x % 2 !== 0 && y % 2 !== 0){
+        document.body.style.backgroundColor = 'blue';
+        console.log(x, y);
+    } else if (x % 2 !== 0 || y % 2 !== 0){
+        document.body.style.backgroundColor = 'green';
+        console.log(x, y);
+    }
 })
