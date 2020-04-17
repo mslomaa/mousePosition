152da99cb1f6f92baf65b1bdab64d85baa43cd29
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-17 14:22:26 +0200

    change bcg color when you move mouse

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ document.body.addEventListener('mousemove', () => {
     // console.log(event.clientX, event.clientY);
     h1.textContent = `${event.clientX}, ${event.clientY}`;
 
-    document.body.style.backgroundColor = ``
+    document.body.style.backgroundColor = `rgb(${event.clientX*2}, ${event.clientY}, ${event.clientX/event.clientY*30})`
 })
